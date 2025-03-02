3d72574913ae0187ac2ece17cd1065e4a7384cc0
MariaElinorac <karolfcjc@gmail.com>
2025-03-02 20:42:37 -0300

    att

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -3,16 +3,13 @@ const AboutContainer = () => {
       <section className="about-container">
         <h2>Sobre</h2>
         <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, dolores!
-          Repellendus eius consequuntur, voluptate deserunt perferendis mollitia
-          debitis est autem aperiam necessitatibus beatae assumenda illo rem
-          architecto quis dolor quae.
+        Sou Maria Caroline Adriano, residente em Bauru, SP, com 7 anos de experiência no setor financeiro,
+        bancário e de suporte ao cliente. Atualmente, trabalho como Operadora de Teleatendimento na Paschoalotto.
         </p>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos veritatis
-          aperiam, odit nihil placeat, dolorum sequi reprehenderit fugiat,
-          distinctio provident impedit modi neque sapiente voluptate voluptates.
-          Maiores sint at dolores?
+          Graduada em Tecnologia em Banco de Dados pela Universidade Nove de Julho e cursando Ciência da Computação 
+          pela Anhanguera Educacional, tenho conhecimentos em HTML, CSS, JavaScript e Python focado em desenvolvimento web.
+Busco oportunidades de estágio na área de tecnologia para aplicar meu conhecimento técnico e crescer profissionalmente.
         </p>
       </section>
     );
