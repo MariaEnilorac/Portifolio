ca59eb41848ff285e9898edb468d3e602c24a398
MariaElinorac <karolfcjc@gmail.com>
2025-03-03 17:26:57 -0300

    att

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -2,15 +2,20 @@ import "../styles/components/projects.sass";
 import "../styles/components/sidebar.sass";
 
 import Ismalia from "../videos/gerenciador-de-projetos.mp4"; 
+import GerenciadordeProjetos from "../videos/gerenciador-de-projetos.mp4"; 
+import ConsultadeClima from "../videos/consulta-de-clima.mp4"; 
+import GerenciadordeTarefas from "../videos/gerenciador-de-tarefas.mp4"; 
+import ListadeTarefas from "../videos/lista-de-tarefas.mp4"; 
+
 
 function ProjectsPage() {
   const projects = [
     { id: 1, title: "Poema Ismália", description: "Descrição do Projeto 1", videoUrl: Ismalia },
-    { id: 2, title: "Gerenciador de Projetos", description: "Descrição do Projeto 2", videoUrl: "https://www.youtube.com/embed/abc" },
-    { id: 3, title: "Consulta de clima", description: "Descrição do Projeto 3", videoUrl: "https://www.youtube.com/embed/xyz" },
-    { id: 4, title: "Gerenciador de tarefas", description: "Descrição do Projeto 4", videoUrl: "https://www.youtube.com/embed/xyz" },
-    { id: 5, title: "Lista de Tarefas", description: "Descrição do Projeto 5", videoUrl: "https://www.youtube.com/embed/xyz" },
-    { id: 6, title: "Projeto 6", description: "Descrição do Projeto 6", videoUrl: "https://www.youtube.com/embed/xyz" },
+    { id: 2, title: "Gerenciador de Projetos", description: "Descrição do Projeto 2", videoUrl: GerenciadordeProjetos },
+    { id: 3, title: "Consulta de clima", description: "Descrição do Projeto 3", videoUrl: ConsultadeClima },
+    { id: 4, title: "Gerenciador de tarefas", description: "Descrição do Projeto 4", videoUrl: GerenciadordeTarefas },
+    { id: 5, title: "Lista de Tarefas", description: "Descrição do Projeto 5", videoUrl: ListadeTarefas },
+    { id: 6, title: "Projeto 6", description: "Descrição do Projeto 6", videoUrl: Ismalia },
   ];
 
   return (
